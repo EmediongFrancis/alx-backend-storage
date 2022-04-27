0593f78540e00593fbac504e9022cc1d1e3f6f85
Emediong ‘Bendito’ Francis <emediongfrancis@gmail.com>
2022-04-27 16:03:13 +0100

    Write string to redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from ctypes import Union
+from uuid import uuid4
+import redis
+
+'''
+    Writing strings to Redis.
+'''
+
+
+class Cache:
+    '''
+        Cache class.
+    '''
+    def __init__(self):
+        '''
+            Initialize the cache.
+        '''
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        '''
+            Store data in the cache.
+        '''
+        randomKey = str(uuid4())
+        self._redis.set(randomKey, data)
+        return randomKey
