08714e20cca98d01d0c07f78787990eebc0c427c
Emediong ‘Bendito’ Francis <emediongfrancis@gmail.com>
2022-04-30 08:56:21 +0100

    Decorate method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -82,6 +82,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''
